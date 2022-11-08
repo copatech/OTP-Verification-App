003ec90e6874fa5f830afa7de06b650106c8b7c9
ADEDOYIN OKUTADE <41125224+copatech@users.noreply.github.com>
2022-11-08 07:55:53 +0100

    Update otp.py

--- otp.py
+++ otp.py
@@ -6,13 +6,13 @@ import smtplib
 nums = '0123456789'
 OTP = ''
 for i in range(6):
-    OTP += nums[math.floor(random.random()*10)]
+    OTP += nums[math.floor(random.random()*10)]          # Genrates the random num
 a = OTP + ' is your unique code'
 token = a
 
-smtp = smtplib.SMTP('smtp.gmail.com', 587)
+smtp = smtplib.SMTP('smtp.gmail.com', 587)              # Gmail smpt sever for sending mails
 smtp.starttls()
-smtp.login('Enter your gmail', 'Enter your google app password')
+smtp.login('Enter your gmail', 'Enter your google app password')        # Here enter your gmail account, and google app password 
 users_id = input('Enter your email: ')
 smtp.sendmail('&&&&&&&&&&', users_id, token)
 x = input('Enter your OTP code >> ')
